2c19e500d1d6d9497eb237cdfc6d0ba4d3d3f770
Fabian Schebera <fabian.schebera@googlemail.com>
2020-10-24 13:24:45 +0200

    wip: adds firebase init

--- backend/lib/firebase.ts
+++ backend/lib/firebase.ts
@@ -1,5 +1,20 @@
 import { initializeApp } from 'firebase';
+import { app } from 'firebase/app';
+import { Observable } from 'rxjs';
+import logger from './logger';
 
-const firebase = initializeApp({
-    
-})
+
+class Firebase {
+    private firebase?: app.App;
+    constructor() {
+        try {
+            this.firebase = initializeApp(JSON.parse(process.env.FIREBASE_CONFIG as string));
+        } catch {
+            logger.error('Unable to initialize firebase! Check the README to learn how to setup the firebase config.');
+        }
+    }
+
+    public get<T extends unknown>(): Observable<T> {
+        
+    }
+}
