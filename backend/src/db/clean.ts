c7c39032d5bbdc67da76828cb1edb8dcfb1f717f
realdegrees <>
2023-09-17 12:29:10 +0200

    improve db cleanup and add notification for servers when data gets deleted

--- backend/src/db/clean.ts
+++ backend/src/db/clean.ts
@@ -1,19 +1,27 @@
-import { Client } from 'discord.js';
+import { Client, Colors } from 'discord.js';
 import { DBGuild } from '../common/types/dbGuild';
 import logger from '../../lib/logger';
 import Database from './database';
+import { NotificationHandler } from '../notificationHandler';
 
 /**
  * @returns deleted guilds
  */
 export const cleanGuilds = async (client: Client, guilds: DBGuild[]): Promise<string[]> => {
-    return client.guilds.fetch().then((clientGuilds) => Promise.all(guilds.map((dbGuild) => {
+    return client.guilds.fetch().then((clientGuilds) => Promise.all(guilds.map(async (dbGuild) => {
         const guild = clientGuilds.find((guild) => guild.id === dbGuild.id);
         if (guild) {
             if (dbGuild.lastActivityTimestamp) {
                 const inactiveDurationDays = (Date.now() - dbGuild.lastActivityTimestamp) / 1000 / 60 / 60 / 24;
                 logger.info(`[${dbGuild.name}] Last Activity ${(inactiveDurationDays * 24).toFixed(2)} hours ago`);
-                if (inactiveDurationDays > 48) {
+                if (inactiveDurationDays > 31) {
+                    await guild.fetch().then((guild) =>
+                        NotificationHandler.sendNotification(guild,
+                            'Data Deletion',
+                            'The bot has been inactive for a month on this server.\nAll saved data will be deleted, you can still use the bot at any time but will have to redo any settings.',
+                            Colors.DarkRed
+                        ))
+
                     return Database.deleteGuild(dbGuild.id).then(() => `${dbGuild.name} (Inactive ${inactiveDurationDays}d)`);
                 } else {
                     return Promise.resolve();
@@ -22,6 +30,7 @@ export const cleanGuilds = async (client: Client, guilds: DBGuild[]): Promise<st
                 return Promise.resolve();
             }
         } else {
+            logger.info(`[${dbGuild.name}] Server removed from DB as Bot is not present on server`);
             return Database.deleteGuild(dbGuild.id).then(() => dbGuild.name);
         }
     }))).then((guildsStates) => guildsStates.filter((state) => !!state) as string[]);
