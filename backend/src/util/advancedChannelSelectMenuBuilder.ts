693ba69b81b4f95987bdc51cbf749856a2fb10e3
realdegrees <18383355+realdegrees@users.noreply.github.com>
2024-07-26 04:20:48 +0200

    adds advancedchannelselector skeleton

--- backend/src/util/advancedChannelSelectMenuBuilder.ts
+++ backend/src/util/advancedChannelSelectMenuBuilder.ts
@@ -1,34 +1,39 @@
-// import { StringSelectMenuOptionBuilder } from '@discordjs/builders';
-// import { APISelectMenuOption, ActionRowBuilder, ButtonBuilder, ChannelSelectMenuBuilder, RestOrArray, SelectMenuComponentOptionData, StringSelectMenuBuilder } from 'discord.js';
+import { StringSelectMenuOptionBuilder } from '@discordjs/builders';
+import { APISelectMenuOption, ActionRowBuilder, ButtonBuilder, ChannelSelectMenuBuilder, ChannelType, RestOrArray, SelectMenuComponentOptionData, StringSelectMenuBuilder } from 'discord.js';
 
-// export class AdvancedChannelSelectMenuBuilder implements
-//     Pick<StringSelectMenuBuilder, 'addOptions' | 'setCustomId' | 'setOptions' | 'setPlaceholder' | 'setDisabled'>,
-//     Pick<ChannelSelectMenuBuilder, 'setChannelTypes'> {
+export class AdvancedChannelSelectMenuBuilder implements
+    Pick<StringSelectMenuBuilder, 'addOptions' | 'setCustomId' | 'setOptions' | 'setPlaceholder' | 'setDisabled'>,
+    Pick<ChannelSelectMenuBuilder, 'setChannelTypes'> {
 
-//     private readonly options: StringSelectMenuOptionBuilder[];
-//     private page: number = 0;
-//     public constructor() { }
-//     public addOptions(...options: RestOrArray<StringSelectMenuOptionBuilder | SelectMenuComponentOptionData | APISelectMenuOption>): StringSelectMenuBuilder {
-//         throw new Error('Method not implemented.');
-//     }
-//     public setCustomId(customId: string): StringSelectMenuBuilder {
-//         throw new Error('Method not implemented.');
-//     }
-//     public setOptions(...options: RestOrArray<StringSelectMenuOptionBuilder | SelectMenuComponentOptionData | APISelectMenuOption>): StringSelectMenuBuilder {
-//         throw new Error('Method not implemented.');
-//     }
-//     public setPlaceholder(placeholder: string): StringSelectMenuBuilder {
-//         throw new Error('Method not implemented.');
-//     }
-//     public setDisabled(disabled?: boolean | undefined): StringSelectMenuBuilder {
-//         throw new Error('Method not implemented.');
-//     }
-//     public setPage(number: number): StringSelectMenuBuilder {
-//     }
-//     public getSelectionRow(): ActionRowBuilder<StringSelectMenuBuilder> {
-
-//     }
-//     public getPageButtonRow(): ActionRowBuilder<ButtonBuilder> {
-
-//     }
-// }
+    private readonly options: StringSelectMenuOptionBuilder[] = [];
+    private page: number = 0;
+    public constructor() { }
+    public setChannelTypes(...types: RestOrArray<ChannelType>): ChannelSelectMenuBuilder {
+        throw new Error('Method not implemented.');
+    }
+    public addOptions(...options: RestOrArray<StringSelectMenuOptionBuilder | SelectMenuComponentOptionData | APISelectMenuOption>): StringSelectMenuBuilder {
+        throw new Error('Method not implemented.');
+    }
+    public setCustomId(customId: string): StringSelectMenuBuilder {
+        throw new Error('Method not implemented.');
+    }
+    public setOptions(...options: RestOrArray<StringSelectMenuOptionBuilder | SelectMenuComponentOptionData | APISelectMenuOption>): StringSelectMenuBuilder {
+        throw new Error('Method not implemented.');
+    }
+    public setPlaceholder(placeholder: string): StringSelectMenuBuilder {
+        throw new Error('Method not implemented.');
+    }
+    public setDisabled(disabled?: boolean | undefined): StringSelectMenuBuilder {
+        throw new Error('Method not implemented.');
+    }
+    public setPage(number: number): StringSelectMenuBuilder {
+        throw new Error('Method not implemented.');
+    }
+    public getSelectionRow(): ActionRowBuilder<StringSelectMenuBuilder> {
+        throw new Error('Method not implemented.');
+    }
+    public getPageButtonRow(): ActionRowBuilder<ButtonBuilder> | undefined {
+        if(this.options.length <= 25) return undefined;
+        
+    }
+}
