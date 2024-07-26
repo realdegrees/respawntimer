7bb489b9d9c42d06aabd0e75370734e6fbe0b088
realdegrees <18383355+realdegrees@users.noreply.github.com>
2024-07-26 04:20:48 +0200

    split text, audio and interval

--- backend/src/audioManager.ts
+++ backend/src/audioManager.ts
@@ -1,12 +1,11 @@
 import {
-    AudioPlayerStatus,
-    AudioResource, createAudioPlayer,
-    createAudioResource, PlayerSubscription, VoiceConnection
+    createAudioPlayer,
+    createAudioResource, getVoiceConnection, PlayerSubscription, VoiceConnection
 } from '@discordjs/voice';
 import fs, { createReadStream } from 'fs';
 import path from 'path';
-import { setTimeout } from 'timers/promises';
-import logger from '../lib/logger';
+import { timers } from './common/timer';
+import { WarInfo } from './common/types';
 
 const loadFiles = (): {
     id: string;
@@ -45,39 +44,73 @@ const loadFiles = (): {
     return sounds;
 };
 
-const volume = 0.7;
 const subscribers: {
+    timeStamp: number;
     guildId: string;
     connection: VoiceConnection;
+    onUnsubscribe: () => void;
     subscription?: PlayerSubscription;
 }[] = [];
 const sounds = loadFiles();
 
 class AudioManager {
-    public constructor(private player = createAudioPlayer()) { }
+    public constructor(private player = createAudioPlayer()) {     }
 
-    public playCountdown(timestamp: number): void {
+    public interval(info: WarInfo): void {
+
+        subscribers.forEach((subscriber) => {
+            // Toggle widget & voice off at war end if it's been on for more than 15 minutes
+            if (info.war.timeLeftSeconds === 5) {
+                const minutesSubscribed = new Date(Date.now() - subscriber.timeStamp).getMinutes();
+                if (minutesSubscribed >= 15) {
+                    this.unsubscribe(subscriber.guildId);
+                }
+            }
+        });
+
+
+        if (info.war.timeLeftSeconds === 1800 - 2) {
+            this.playStart();
+        }
+
+        // Audioplayer only plays at the second marks provided by available sound files
+        // Skip any announcements higher than 50% of the total time 
+        if (info.respawn.timePassed / info.respawn.duration < 0.50 && info.respawn.remaining > 0) {
+            this.playCountdown(info.respawn.timePassed);
+        }
+        if (info.respawn.remaining <= 5 && info.respawn.duration - info.respawn.timePassed === 2) {
+            this.playRespawnCount(info.respawn.remaining);
+        }
+        if (info.respawn.remaining === 0 && 1800 - timers[timers.length - 1] - info.war.timeLeftSeconds === 5) {
+            // Plays last respawn sound
+            this.playRespawnCount(0);
+        }
+    }
+
+    private playCountdown(timestamp: number): void {
         const sound = sounds.find((sound) => sound.id === timestamp.toString());
         if (sound) {
             this.player.play(createAudioResource(createReadStream(sound.path)));
         }
     }
-    public playRespawnCount(count: number): void {
+    private playRespawnCount(count: number): void {
         const sound = sounds.find((sound) => sound.id === 'respawn-' + count);
         if (sound) {
             this.player.play(createAudioResource(createReadStream(sound.path)));
         }
     }
-    public playStart(): void {
+    private playStart(): void {
         const sound = sounds.find((sound) => sound.id === 'start');
         if (sound) {
             this.player.play(createAudioResource(createReadStream(sound.path)));
         }
     }
-    public subscribe(connection: VoiceConnection, guildId: string): void {
+    public subscribe(connection: VoiceConnection, guildId: string, onUnsubscribe: () => void): void {
         subscribers.push({
+            timeStamp: Date.now(),
             guildId,
             connection,
+            onUnsubscribe,
             subscription: connection.subscribe(this.player)
         });
 
@@ -99,8 +132,11 @@ class AudioManager {
     }
     public unsubscribe(guildId: string): void {
         const subscriber = subscribers.find((s) => s.guildId === guildId);
+        subscriber?.onUnsubscribe();
         subscriber?.subscription?.unsubscribe();
         subscribers.splice(subscribers.findIndex((s) => s.guildId === guildId), 1);
+        getVoiceConnection(guildId)?.disconnect();
+        getVoiceConnection(guildId)?.destroy();
     }
 }
 export default new AudioManager();
