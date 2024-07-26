ab5527c5a46568eaac04a6724dc098fe49ec570a
realdegrees <18383355+realdegrees@users.noreply.github.com>
2024-07-26 04:20:48 +0200

    fixes failed promise when multiple permissions are not given

--- backend/src/util/checkChannelPermissions.ts
+++ backend/src/util/checkChannelPermissions.ts
@@ -2,10 +2,10 @@ import { GuildBasedChannel, PermissionResolvable } from 'discord.js';
 
 
 export const checkChannelPermissions = (channel: GuildBasedChannel, permissions: PermissionResolvable[]): Promise<void> => {
-    permissions.forEach((permission) => {
+    for (const permission of permissions) {
         if (!channel.permissionsFor(channel.client.user)?.has(permission)) {
             return Promise.reject(`Missing *${permission}* permission in ${channel}`);
         }
-    });
+    }
     return Promise.resolve();
 };
